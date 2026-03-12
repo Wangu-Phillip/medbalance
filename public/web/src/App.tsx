0fc1fe080c77367cd3eb091f323a65a465ad3f1a
Wangu-Phillip <wanguphillip@gmail.com>
2026-03-12 08:01:17 +0200

    Fix: deploying to hosting

--- public/web/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import LandingPage from "./pages/LandingPage";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-slate-50 text-slate-900">
-      <LandingPage />
-    </div>
-  );
-}
